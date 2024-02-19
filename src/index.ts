351f255a93be3b67a58e6cf67decc59fd7f3e6da
Ido <gershido@gmail.com>
2024-02-19 13:01:26 +0200

    executions support

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
 import { HatsAccount1ofNClient } from "./clients";
-import type { CreateAccountResult } from "./types";
+import {
+  HATS_ACCOUNT_1OFN_ABI,
+  ERC6551_REGISTRY_ABI,
+  HATS_ACCOUNT_1OFN_IMPLEMENTATION,
+  ERC6551_REGISTRY,
+  OperationType,
+} from "./constants";
+import type { CreateAccountResult, ExecutionResult, Operation } from "./types";
 
-export { HatsAccount1ofNClient };
-export type { CreateAccountResult };
+export {
+  HatsAccount1ofNClient,
+  HATS_ACCOUNT_1OFN_ABI,
+  ERC6551_REGISTRY_ABI,
+  HATS_ACCOUNT_1OFN_IMPLEMENTATION,
+  ERC6551_REGISTRY,
+  OperationType,
+};
+export type { CreateAccountResult, ExecutionResult, Operation };
--- test/hatsAccount1ofN.test.ts
+++ test/hatsAccount1ofN.test.ts
@@ -1,11 +1,22 @@
-import { HatsAccount1ofNClient } from "../src";
-import { HatsClient } from "@hatsprotocol/sdk-v1-core";
+import { HatsAccount1ofNClient, OperationType } from "../src";
+import {
+  HatsClient,
+  HATS_V1,
+  hatIdToTreeId,
+  treeIdToTopHatId,
+} from "@hatsprotocol/sdk-v1-core";
 import { createPublicClient, createWalletClient, http } from "viem";
 import { sepolia } from "viem/chains";
 import { createAnvil } from "@viem/anvil";
 import { privateKeyToAccount } from "viem/accounts";
-import type { PublicClient, WalletClient, PrivateKeyAccount } from "viem";
+import type {
+  PublicClient,
+  WalletClient,
+  PrivateKeyAccount,
+  Address,
+} from "viem";
 import type { Anvil } from "@viem/anvil";
+import type { ExecutionResult, Operation } from "../src";
 import "dotenv/config";
 
 describe("Client Tests", () => {
@@ -16,6 +27,7 @@ describe("Client Tests", () => {
   let anvil: Anvil;
 
   let account1: PrivateKeyAccount;
+  let account2: PrivateKeyAccount;
   let hat1: bigint;
   let hat1_1: bigint;
 
@@ -28,6 +40,10 @@ describe("Client Tests", () => {
 
     account1 = privateKeyToAccount(
       "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+    );
+
+    account2 = privateKeyToAccount(
+      "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d"
     );
 
     // init Viem clients
@@ -70,22 +86,160 @@ describe("Client Tests", () => {
       account: account1,
     });
     hat1_1 = resHat1_1.hatId;
-  }, 30000);
 
-  test("Test hats account creation", async () => {
-    const predictedHatsAccount = await hatsAccountClient.predictAccountAddress({
-      hatId: hat1_1,
-      salt: 1n,
-    });
-
-    const res = await hatsAccountClient.createAccount({
+    await hatsClient.mintHat({
       account: account1,
       hatId: hat1_1,
-      salt: 1n,
+      wearer: account1.address,
     });
-    const createdHatsAccount = res.newAccount;
+  }, 30000);
 
-    expect(predictedHatsAccount).toBe(createdHatsAccount);
+  describe("Create new account", () => {
+    let hatsAccountInstance: Address;
+
+    beforeAll(async () => {
+      const res = await hatsAccountClient.createAccount({
+        account: account1,
+        hatId: hat1_1,
+        salt: 1n,
+      });
+
+      hatsAccountInstance = res.newAccount;
+    });
+
+    test("Test hats account creation", async () => {
+      const predictedHatsAccount =
+        await hatsAccountClient.predictAccountAddress({
+          hatId: hat1_1,
+          salt: 1n,
+        });
+
+      expect(predictedHatsAccount).toBe(hatsAccountInstance);
+    });
+
+    describe("Execute from account", () => {
+      let res: ExecutionResult;
+
+      beforeAll(async () => {
+        const calldata = hatsClient.mintTopHatCallData({
+          target: hatsAccountInstance,
+          details: "New Top Hat",
+          imageURI: "New Image URI",
+        });
+
+        res = await hatsAccountClient.execute({
+          account: account1,
+          instance: hatsAccountInstance,
+          operation: {
+            to: HATS_V1,
+            value: 0n,
+            data: calldata.callData,
+            operation: OperationType.Call,
+          },
+        });
+      });
+
+      test("Test execution", async () => {
+        const predictedTopHatId = treeIdToTopHatId(hatIdToTreeId(hat1) + 1);
+        const isWearer = await hatsClient.isWearerOfHat({
+          wearer: hatsAccountInstance,
+          hatId: predictedTopHatId,
+        });
+
+        expect(res.status).toBe("success");
+        expect(isWearer).toBe(true);
+      });
+
+      test("Reverts with invalid signer", async () => {
+        await expect(async () => {
+          await hatsAccountClient.execute({
+            account: account2,
+            instance: hatsAccountInstance,
+            operation: {
+              to: account2.address,
+              value: 1n,
+              data: "0x",
+              operation: OperationType.Call,
+            },
+          });
+        }).rejects.toThrow(
+          "Error: calling account is not wearing the required hat"
+        );
+      });
+
+      test("Reverts with invalid operation", async () => {
+        await expect(async () => {
+          await hatsAccountClient.execute({
+            account: account1,
+            instance: hatsAccountInstance,
+            operation: {
+              to: account2.address,
+              value: 0n,
+              data: "0x",
+              operation: 3 as unknown as OperationType,
+            },
+          });
+        }).rejects.toThrow(
+          "Error: only the call or delegatecall operations are supported"
+        );
+      });
+    });
+
+    describe("Batch execute from account", () => {
+      let res: ExecutionResult;
+
+      beforeAll(async () => {
+        const calldata1 = hatsClient.mintTopHatCallData({
+          target: hatsAccountInstance,
+          details: "New Top Hat 1",
+          imageURI: "New Image URI 1",
+        });
+
+        const calldata2 = hatsClient.mintTopHatCallData({
+          target: hatsAccountInstance,
+          details: "New Top Hat 2",
+          imageURI: "New Image URI 2",
+        });
+
+        const operations: Operation[] = [
+          {
+            to: HATS_V1,
+            value: 0n,
+            data: calldata1.callData,
+            operation: OperationType.Call,
+          },
+          {
+            to: HATS_V1,
+            value: 0n,
+            data: calldata2.callData,
+            operation: OperationType.Call,
+          },
+        ];
+
+        res = await hatsAccountClient.executeBatch({
+          account: account1,
+          instance: hatsAccountInstance,
+          operations,
+        });
+      });
+
+      test("Test execution", async () => {
+        const predictedTopHatId1 = treeIdToTopHatId(hatIdToTreeId(hat1) + 2);
+        const predictedTopHatId2 = treeIdToTopHatId(hatIdToTreeId(hat1) + 3);
+        const isWearer1 = await hatsClient.isWearerOfHat({
+          wearer: hatsAccountInstance,
+          hatId: predictedTopHatId1,
+        });
+        const isWearer2 = await hatsClient.isWearerOfHat({
+          wearer: hatsAccountInstance,
+          hatId: predictedTopHatId2,
+        });
+
+        expect(res.status).toBe("success");
+        expect(isWearer1).toBe(true);
+        expect(isWearer2).toBe(true);
+      });
+    });
   });
 
   afterAll(async () => {
